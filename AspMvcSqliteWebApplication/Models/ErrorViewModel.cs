d9265cdeb041e093bd48657a10739c2aa7f947f4
chvsrc <chvsrc@gmail.com>
2023-10-11 22:29:16 -0400

    Added AspMvcSqliteWebApplication

---
+++ AspMvcSqliteWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspMvcSqliteWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
