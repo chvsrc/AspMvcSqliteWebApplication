6f724cb1320849f940076a98f2bd1b570cd7e561
chvsrc <chvsrc@gmail.com>
2023-10-11 22:52:55 -0400

    Added Company DbContext

---
+++ AspMvcSqliteWebApplication/DatabaseContexts/CompanyContext.cs
@@ -0,0 +1,26 @@
+﻿
+using AspMvcSqliteWebApplication.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspMvcSqliteWebApplication.DatabaseContexts
+{
+    public class CompanyContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        public string DbPath { get; }
+
+        public CompanyContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "blogging.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
